e714f4ffcb43b96d686eb11b28667cadd71edc35
TheCraftyRobot <47324936+TheCraftyRobot@users.noreply.github.com>
2020-06-08 18:19:12 +0100

    Remapped motors to match stock Smartibot

--- js/firmware.js
+++ js/firmware.js
@@ -1,85 +1,87 @@
 var smarti = require("Smartibot");
 var counting = false;
-var looking = false;
+var lookUp = false;
+var lookDw = false;
+var lookSt = false;
 var count = 0;
 var step = 2;
 
 function go(drive){
-if (drive == "F"){
+if (drive == "B"){
  smarti.setLEDs([0,0,50],[0,0,50]);
- smarti.setMotor(3,0.7);
- smarti.setMotor(4,-0.7);
-  step = 4;
+ smarti.setMotor(1,0.7);
+ smarti.setMotor(2,-0.7);
+  step = 8;
  counting = true;
- }
- if (drive == "L"){
- smarti.setLEDs([50,0,50],[60,0,50]);
- smarti.setMotor(3,-0.6);
- smarti.setMotor(4,-0.6);
-   step = 2;
- counting = true;
-
- 
- }
-
-  if (drive == "SL"){
- smarti.setLEDs([50,0,50],[60,0,50]);
-  smarti.setMotor(3,-0.55);
- smarti.setMotor(4,-0.55);
-   step = 1;
- counting = true;
-
- 
  }
  if (drive == "R"){
- smarti.setLEDs([50,0,50],[50,0,50]);
- smarti.setMotor(3,0.6);
- smarti.setMotor(4,0.6);
+ smarti.setLEDs([50,0,50],[60,0,50]);
+ smarti.setMotor(1,-0.6);
+ smarti.setMotor(2,-0.6);
+   step = 4;
+ counting = true;
+
+ 
+ }
+
+  if (drive == "SR"){
+ smarti.setLEDs([50,0,50],[60,0,50]);
+  smarti.setMotor(1,-0.55);
+ smarti.setMotor(24,-0.55);
    step = 2;
  counting = true;
+
+ 
  }
-  if (drive == "SR"){
+ if (drive == "L"){
  smarti.setLEDs([50,0,50],[50,0,50]);
- smarti.setMotor(3,0.55);
- smarti.setMotor(4,0.55);
-   step = 1;
+ smarti.setMotor(1,0.6);
+ smarti.setMotor(2,0.6);
+   step = 4;
+ counting = true;
+ }
+  if (drive == "SL"){
+ smarti.setLEDs([50,0,50],[50,0,50]);
+ smarti.setMotor(1,0.55);
+ smarti.setMotor(2,0.55);
+   step = 2;
  counting = true;
  }
  
  if (drive == "LU"){
- smarti.setMotor(2,-0.8);
-  step = 1;
+ smarti.setMotor(4,0.9);
+  step = 2;
  counting = true;
- looking= true;
+ lookUp= true;
  }
 
    if (drive == "LD"){
- smarti.setMotor(2, 0.6);
-  step = 1;
+ smarti.setMotor(4,-0.80);
+  step = 4;
  counting = true;
- looking = true;
+ lookDw = true;
  }
   
   if (drive == "T"){
  smarti.setLEDs([50,0,50],[50,0,50]);
- smarti.setMotor(3,0.55);
- smarti.setMotor(4,0.55);
-   step = 15;
+ smarti.setMotor(1,0.55);
+ smarti.setMotor(2,0.55);
+   step = 30;
  counting = true;
  }
 
 
- else if (drive == "B"){
+ else if (drive == "F"){
  smarti.setLEDs([50,0,0],[50,0,0]);
- smarti.setMotor(3,-0.7);
- smarti.setMotor(4,0.7);
-   step = 4;
+ smarti.setMotor(1,-0.7);
+ smarti.setMotor(2,0.7);
+   step = 8;
  counting = true;
  }
  else if (drive == "S"){
  smarti.setLEDs([0,50,0],[0,50,0]);
- smarti.setMotor(3,0);
- smarti.setMotor(4,0);
+ smarti.setMotor(1,0);
+ smarti.setMotor(2,0);
  } 
 }
 
@@ -89,18 +91,32 @@ setInterval(function() {
  }
  if (count > step){
    
- if(looking == true){
-   smarti.setMotor(2,-0.3);
-   step = 3;
+ if(lookUp == true){
+   smarti.setMotor(4,0.5);
+   step = 1;
    count = 0;
-   looking = false;
+   lookUp = false;
+   lookSt = true;
+ }
+  else if(lookDw == true){
+   smarti.setMotor(4,0.5);
+   step = 1;
+   count = 0;
+   lookDw = false;
+   lookSt = true;
+ }
+   else if(lookSt == true){
+   smarti.setMotor(4,0.30);
+   step = 15;
+   count = 0;
+   lookSt = false;
  }
 else{ 
  counting = false;
  count = 0;
+ smarti.setMotor(1,0);
  smarti.setMotor(2,0);
- smarti.setMotor(3,0);
  smarti.setMotor(4,0);
 }
  }
-}, 50);
+}, 25);
